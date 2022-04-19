6e1c10127c109e5a17bb027172cba607c031bd68
Arc-Data <arclight04@gmail.com>
2022-04-19 08:48:09 +0800

    Profile Model setup

--- social/models.py
+++ social/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save 
+from django.dispatch import receiver
 
-# Create your models here.
+class Profile(models.Model):
+	user = models.OneToOneField(User, on_delete = models.CASCADE)
+	username = models.CharField(max_length = 25, blank = True, null = True)
+	profile_pic = models.ImageField(upload_to = 'uploads/profile_pictures', default = "uploads/profile_pictures/default-blue.png")
+	banner_pic = models.ImageField(upload_to = 'uploads/banner_pictures', default = "uploads/banner_pictures/740377.png")
+
+	def __str__(self):
+		return self.username
+
+@receiver(post_save, sender = User)
+def create_profile(sender, instance, created, **kwargs):
+	if created:
+		Profile.objects.create(
+			user = instance,
+			username = instance.username
+			)
+		print('Profile Attached')
+
+@receiver(post_save, sender = User)
+def save_user_profile(sender, instance, **kwargs):
+	instance.profile.save()
