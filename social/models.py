b1dca06cfd8d96da8801eac554a54e75a9e4e2e7
RikuDola <85489963+Arc-Data@users.noreply.github.com>
2022-04-20 10:03:52 +0800

    Basic Profile Details Completed (#5)
    * Slugified Username
    
    * Profile View Template Rendered
    
    * Profile Banner Created
    
    * Profile Link for Editing and Display Updated

--- social/models.py
+++ social/models.py
@@ -1,13 +1,20 @@
 from django.db import models
+from django.utils import timezone
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save 
 from django.dispatch import receiver
+from django.utils.text import slugify
+
+
+from PIL import Image
 
 class Profile(models.Model):
 	user = models.OneToOneField(User, on_delete = models.CASCADE)
-	username = models.CharField(max_length = 25, blank = True, null = True)
+	username = models.CharField(max_length = 25, blank = True, null = True, unique =True)
 	profile_pic = models.ImageField(upload_to = 'uploads/profile_pictures', default = "uploads/profile_pictures/default-blue.png")
 	banner_pic = models.ImageField(upload_to = 'uploads/banner_pictures', default = "uploads/banner_pictures/740377.png")
+	date_joined = models.DateField(editable = False)
+	slug = models.SlugField(unique = True)
 
 	def __str__(self):
 		return self.username
@@ -17,7 +24,9 @@ def create_profile(sender, instance, created, **kwargs):
 	if created:
 		Profile.objects.create(
 			user = instance,
-			username = instance.username
+			username = instance.username,
+			slug = slugify(instance.username),
+			date_joined = timezone.now()
 			)
 		print('Profile Attached')
 
--- social/views.py
+++ social/views.py
@@ -2,11 +2,20 @@ from django.shortcuts import render
 from django.views.generic import View 
 from django.contrib.auth.mixins import LoginRequiredMixin
 
+from .models import Profile
 
 class HomeView(LoginRequiredMixin, View):
 	def get(self, request, *args, **kwargs):
 
 		return render(request, 'social/home.html', {})
 
+
+class ProfileView(LoginRequiredMixin, View):
+	def get(self, request, slug, *args, **kwargs):
+		profile = Profile.objects.get(slug = slug)
+		context = {
+			'profile':profile,
+		}
+		return render(request, 'social/profile.html', context)
 
 # Create your views here.
