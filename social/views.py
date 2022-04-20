3f04deeb0d341263f1b4a77c969ace037f8d1d2c
Arc-Data <arclight04@gmail.com>
2022-04-20 14:32:37 +0800

    Added Create Post Link for Home Page

--- social/views.py
+++ social/views.py
@@ -3,16 +3,21 @@ from django.views.generic import View
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Profile
+from .forms import PostForm
 
 class HomeView(LoginRequiredMixin, View):
 	def get(self, request, *args, **kwargs):
-
-		return render(request, 'social/home.html', {})
+		form = PostForm
+		context = {
+			'form':form,
+		}
+		return render(request, 'social/home.html', context)
 
 
 class ProfileView(LoginRequiredMixin, View):
 	def get(self, request, slug, *args, **kwargs):
 		profile = Profile.objects.get(slug = slug)
+
 		context = {
 			'profile':profile,
 		}
