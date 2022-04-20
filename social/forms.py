794fb8b010a23819abc10e417c43dd5a4f24d55e
Arc-Data <arclight04@gmail.com>
2022-04-20 11:33:06 +0800

    Followup fix to forms.py

--- social/forms.py
+++ social/forms.py
@@ -1,1 +1,10 @@
-p
+from django import forms
+from django.forms import ModelForm 
+
+from .models import Post, Profile 
+
+
+class PostForm(forms.ModelForm):
+	class Meta:
+		model = Post 
+		fields = ['body']
