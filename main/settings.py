394d43b9e356023d30ad0c35bfb4a685c894c291
Arc-Data <arclight04@gmail.com>
2022-06-15 09:47:27 +0800

    Migrated to MySQL

--- main/settings.py
+++ main/settings.py
@@ -95,8 +95,11 @@ WSGI_APPLICATION = 'main.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'social',
+        'USER': 'riku',
+        'PASSWORD': 'database',
+        'HOST': 'localhost',
     }
 }
 
